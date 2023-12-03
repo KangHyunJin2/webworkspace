41a2f49998a7083b80fd021b717c33c3fae7c62e
KangHyunJin2 <khj0501k@naver.com>
2023-12-04 00:15:10 +0900

    야간작업

--- vue/user-project/src/views/UserList.vue
+++ vue/user-project/src/views/UserList.vue
@@ -23,7 +23,7 @@
     </div>    
 </template>
 <script>
-import axios from 'axios';
+import axios from 'axios'; // 모듈 가져오기
 
 export default {
     data(){
@@ -33,16 +33,18 @@ export default {
     },
     computed : {
         count(){
-            return this.userList.length;
+            return this.userList.length;  // 배열의 길이
         }
     },
     created(){
         // 해당 컴포넌트가 초기화할 데이터관련 진행
-        this.getUserList();
+        this.getUserList(); //this.
     },
     methods : {
+        // 디비 데이터 불러오는 함수
+        //axios의 결과를 객체로 가져옴 
         async getUserList(){
-            let result = await axios.get('/users')
+            let result = await axios.get('/users') //const target = localhost:3000 지정했음 아까
                                 .catch(err => {
                                     console.log(err);
                                 });
--- vue/user-project/vue.config.js
+++ vue/user-project/vue.config.js
@@ -4,9 +4,9 @@ const target = 'http://localhost:3000'
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer : {
-    port : 8080,
+    port : 8080, //개발 도중 적용되는 서버
     proxy : {
-      '^/':{                //통신을 시도할때 
+      '^/':{                //통신을 시도할때 슬러시로 시작하는 모든 경우 프록시 설정 적용하겠다
         target,             // target 속성에 origin으로 대체 하겠다
         changeOrigin : true
       }
